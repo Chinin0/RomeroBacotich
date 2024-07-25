103c7862bf7ad5d49cac81a86fe2bc9c86f9660f
Rauuw <oscarzeballoscarvallo@gmail.com>
2024-07-25 16:47:12 -0400

    add landing

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ use App\Http\Controllers\OfertaController;
 //CARRITO
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::view('/', 'landing')->name('landing');
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
